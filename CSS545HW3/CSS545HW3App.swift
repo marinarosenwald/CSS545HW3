229a0ec3f5ad365a24c5b2f2aa492e4853a90b2a
marinarosenwald <marinarosenwald@gmail.com>
2024-05-03 13:28:27 -0700

    initial commit

---
+++ CSS545HW3/CSS545HW3App.swift
@@ -0,0 +1,17 @@
+//
+//  CSS545HW3App.swift
+//  CSS545HW3
+//
+//  Created by Mari Rosenwald on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CSS545HW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
