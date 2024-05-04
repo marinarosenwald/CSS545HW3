96c5a3f83e9811cf734a29deb4135b8995c5682f
marinarosenwald <marinarosenwald@gmail.com>
2024-05-04 12:43:10 -0700

    takes user input and stores it

--- CSS545HW3/ContentView.swift
+++ CSS545HW3/ContentView.swift
@@ -8,17 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userInput: String = ""
+    
+    func saveUserInput() {
+        UserDefaults.standard.set(userInput, forKey: "UserInput")
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Enter some text:")
+            TextField("Type here", text: $userInput)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Text("You entered: \(userInput)")
+            
         }
         .padding()
+        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+            saveUserInput()
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
